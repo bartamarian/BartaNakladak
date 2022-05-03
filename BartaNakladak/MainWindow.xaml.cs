39d22f93024713249db0e44fa3fc994202f8a90e
Marian Bárta <Barta.Marian@stud.vassboskovice.cz>
2022-05-03 09:00:30 +0200

    Přidejte soubory projektu.

---
+++ BartaNakladak/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BartaNakladak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Nakladak man;
+        Nakladak daf;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            man = new Nakladak();
+            man.Jmeno = "MAN";
+            Zobraz(man, TextBox1);
+
+            daf = new Nakladak();
+            daf.Jmeno = "DAF";
+            Zobraz(daf, TextBox2);
+        }
+
+        public void Zobraz (Nakladak nakladak, TextBox textBox)
+        {
+            textBox.Text = nakladak.Jmeno + "\n";
+            textBox.Text += "Nosnost: " + nakladak.Nosnost.ToString() + "\n";
+            textBox.Text += "Objem nádrže: " + nakladak.ObjemNadrze() + "\n";
+        }
+    }
+}
---
+++ BartaNakladak/Nakladak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BartaNakladak
+{
+    public class Nakladak
+    {
+        public int Nosnost;
+        public int ObjemNadrze;
+        public int SpotrebaPaliva;
+        public int CelkovaUjetaVzdalenost;
+
+        public string Jmeno { get; set; }
+        
+    }
+
+    public Nakladak()
+    {
+        Jmeno = " ";
+        nosnost = 20000;
+        objenadrze = 1000;
+        spotrebapaliva = 30;
+
+
+    }
+}
